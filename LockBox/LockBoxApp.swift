ead38155b93a40f3136b5a01d77cfd8228e6cc1b
brettnguyen <77566307+brettnguyen@users.noreply.github.com>
2024-03-14 15:53:12 -0400

    Initial Commit

---
+++ LockBox/LockBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockBoxApp.swift
+//  LockBox
+//
+//  Created by Brett Nguyen  on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct LockBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
